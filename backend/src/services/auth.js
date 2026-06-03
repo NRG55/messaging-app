acaadc013be71b432cb5ba2b2b38fe5d369be5a4
NRG55 <maloj_1980@yahoo.fr>
2026-06-03 12:52:42 +0100

    feat(backend): initialize postgres database and user prisma model

--- backend/src/services/auth.js
+++ backend/src/services/auth.js
@@ -1,5 +1,13 @@
+import prisma from "../config/prisma.js";
+
 const register = async ({ username, password }) => {
-    const user = { username, password };
+    const user = await prisma.user.create({
+        data: {
+            username,
+            password
+        }
+    });
+
     const token = 'adgshtjjesfef'
 
     return { user, token };
