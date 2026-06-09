1a54f959217d1ed1981b83a84c9dc1df26aba13c
NRG55 <maloj_1980@yahoo.fr>
2026-06-09 11:50:02 +0100

    feat(frontend): install react-router and configure home, register and login routes

---
+++ frontend/src/routes.jsx
@@ -0,0 +1,20 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Reguster";
+
+const routes = [
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/register',
+        element: <Register />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },    
+];
+
+export default routes;
